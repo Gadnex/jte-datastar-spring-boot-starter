392909280bba0e3d3b69934db04d6b620f5394f4
William Gadney <gadnex@gmail.com>
2025-07-03 19:01:39 +0200

    Fix ExecuteScript autoRemove and attributes

--- src/main/java/io/github/gadnex/jtedatastar/ExecuteScript.java
+++ src/main/java/io/github/gadnex/jtedatastar/ExecuteScript.java
@@ -1,8 +1,6 @@
 package io.github.gadnex.jtedatastar;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 /**
@@ -14,6 +12,7 @@ public class ExecuteScript extends AbstractDatastarEmitter {
 
   private final List<String> scripts;
   private Boolean autoRemove;
+  private final StringBuilder attributes = new StringBuilder();
 
   private static final String DATASTAR_PATCH_ELEMENTS = " datastar-patch-elements";
   private static final String SELECTOR = " selector body";
@@ -41,6 +40,28 @@ public class ExecuteScript extends AbstractDatastarEmitter {
   }
 
   /**
+   * Optionally add a script attribute. Each attributes line adds an attribute (in the format name
+   * value) to the script element.
+   *
+   * @param name The attribute name
+   * @param value The attribute value
+   * @return The ExecuteScript object
+   */
+  public ExecuteScript attribute(String name, String value) {
+    if (name == null || name.isBlank()) {
+      throw new IllegalArgumentException("name cannot be null or empty");
+    }
+    attributes.append(" ");
+    attributes.append(name);
+    if (value != null) {
+      attributes.append("=\"");
+      attributes.append(value);
+      attributes.append("\"");
+    }
+    return this;
+  }
+
+  /**
    * Add at least 1 script line. Each script line contains JavaScript to be executed by the browser.
    *
    * @param script the script line
@@ -63,9 +84,13 @@ public class ExecuteScript extends AbstractDatastarEmitter {
     event.data(PatchMode.APPEND.output());
     event.data(SELECTOR);
     if ((autoRemove != null) && autoRemove) {
-      event.data(ELEMENTS_DATALINE_LITERAL + "<script onload=\"this.remove()\">");
+      event.data(
+          ELEMENTS_DATALINE_LITERAL
+              + "<script data-effect=\"el.remove()\""
+              + attributes.toString()
+              + ">");
     } else {
-      event.data(ELEMENTS_DATALINE_LITERAL + "<script>");
+      event.data(ELEMENTS_DATALINE_LITERAL + "<script" + attributes.toString() + ">");
     }
     for (String script : scripts) {
       event.data(ELEMENTS_DATALINE_LITERAL + script);
